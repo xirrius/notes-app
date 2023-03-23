f882c3c6e1aeed78dd9f7bd967cded110459ce96
Shreya Nigam <shreya.nigam1062@gmail.com>
2023-03-23 20:35:56 +0530

    Initial commit

---
+++ src/components/Notes.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Notes() {
+  return (
+    <div className="note">
+      <h1>Memo</h1>
+      <p>
+        This is a memo. You can save valuable information here. Memos help you
+        keep track and be proactive with respect to your tasks. Hope you enjoyed
+        this memo!!
+      </p>
+    </div>
+  );
+}
+
+export default Notes;
